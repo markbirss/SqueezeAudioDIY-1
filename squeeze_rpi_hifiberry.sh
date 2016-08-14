b24063b53fb12c6ab7e90c8d43fa32e022bffe27
ictinus2310 <coen.human@gmail.com>
2016-08-14 12:36:57 +0200

    Update and rename squeeze_hifiberry.sh to squeeze_rpi_hifiberry.sh

---
+++ squeeze_rpi_hifiberry.sh
@@ -0,0 +1,24 @@
+
+#!/bin/sh
+
+#------------------------------------
+#CONFIGURING BOOT.CONFIG
+#------------------------------------
+sed -i 's/#dtparam=i2s=on/dtparam=i2s=on/g' /boot/config.txt
+echo "dtoverlay=hifiberry-dac" >> /boot/config.txt
+tput setaf 3; echo "Configured /boot/config.txt for I2S DAC."
+
+#------------------------------------
+#ADDING MODULES FOR DAC
+#------------------------------------
+echo "snd_soc_bcm2708_i2s" >> /etc/modules
+echo "bcm2708_dmaengine" >> /etc/modules
+echo "snd_soc_pcm5102a" >> /etc/modules
+echo "snd_soc_hifiberry_dac" >> /etc/modules
+tput setaf 3; echo "Modules added to /etc/modules file."
+
+#------------------------------------
+#ADDING MODULES FOR DAC
+#------------------------------------
+tput setaf 3; echo "System needs to reboot for DAC to activate."
+exit
