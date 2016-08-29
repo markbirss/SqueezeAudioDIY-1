762addb06bc6b1937829ab85a1f9b3cf31858d95
ictinus2310 <coen.human@gmail.com>
2016-08-29 17:01:59 +0200

    Add files via upload

--- squeeze_audio.sh
+++ squeeze_audio.sh
@@ -19,14 +19,6 @@ sed -i '1 d' ./available_list.txt #REMOVES FIRST LINE FROM FILE
 sed -i '$ d' ./available_list.txt #REMOVES LAST LINE FROM FILE
 
 #------------------------------------
-#DISPLAY AVAILABLE LIST
-#------------------------------------
-echo "###################################################################"
-echo "   SELECT DEVICE BY ENTERING CORRESPONDING NUMBER NEXT TO DEVICE   "
-echo "###################################################################"
-cat -n ./available_list.txt #LIST LINES WITH LINE NUMBERS
-
-#------------------------------------
 #CREATE DEVICE LIST
 #------------------------------------
 sed 's/-.*//' ./available_list.txt > ./devices.txt #REMOVE EVERYTHING AFTER '-' CHAR
@@ -35,8 +27,17 @@ sed -i -e 's/^[ \t]*//' -e 's/[ \t]*$//' ./devices.txt #REMOVES SPACES FROM FILE
 #------------------------------------
 #SELECT DEVICE
 #------------------------------------
-echo -n "SELECTED DEVICE: "
-read device
+available_list=$(cat -n ./available_list.txt)
+device=$(whiptail --title "Available Devices:" --inputbox "$available_list" 30 140 Number 3>&1 1>&2 2>&3)
+
+exitstatus=$?
+if [ $exitstatus = 0 ]; then
+    echo "You chose:" $device
+else
+    echo "You chose Cancel."
+    exit
+fi
+
 selected_device=$(sed -n "${device}p" ./devices.txt)
 
 #------------------------------------
@@ -49,10 +50,8 @@ sed -i '9s/cha/"/' /etc/default/squeezelite
 #------------------------------------
 #VIEW NEW SETTINGS
 #------------------------------------
-echo "###################################################################"
-echo "                           NEW SETTINGS                            "
-echo "###################################################################"
-cat /etc/default/squeezelite
+name_settings=$(cat /etc/default/squeezelite)
+whiptail --title "Current Settings" --msgbox "$name_settings" 30 100
 
 #------------------------------------
 #START SQUEEZELITE
