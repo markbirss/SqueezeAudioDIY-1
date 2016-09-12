e6c0e9f87457d519586ecff7478b1cc13c65c92f
Ictinus2310 <coen.human@gmail.com>
2016-09-12 12:56:58 +0200

    Updates.

--- scripts/squeeze_update.sh
+++ scripts/squeeze_update.sh
@@ -4,20 +4,21 @@
 #STOP SQUEEZELITE
 #------------------------------------
 service squeezelite stop
+echo "Stopped Squeezelite."
 
 #------------------------------------
 #DIRECTORIES
 #------------------------------------
-rm -R /Squeezelite/squeezelite
-echo "Old files removed."
+rm -R /usr/bin/Squeezelite/squeezelite
+echo "Old Squeezelite removed."
 
 #------------------------------------
 #COMPILE LATEST SQUEEZELITE
 #------------------------------------
 echo "Compiling latest Squeezelite:"
-cd /Squeezelite
+cd /usr/bin/Squeezelite/
 git clone https://github.com/ralph-irving/squeezelite.git
-cd /Squeezelite/squeezelite
+cd /usr/bin/Squeezelite/squeezelite
 OPTS="-DDSD -DRESAMPLER" make
 
 #------------------------------------
