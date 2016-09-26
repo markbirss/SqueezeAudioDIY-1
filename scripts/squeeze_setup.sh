840835f201780b207e0e0bdb7c5fb4e67b6ae8c0
Coenraad Human <coen.human@gmail.com>
2016-09-26 22:39:17 +0200

    Update squeeze_setup.sh

--- scripts/squeeze_setup.sh
+++ scripts/squeeze_setup.sh
@@ -12,9 +12,8 @@ fi
 #------------------------------------
 menu=$(whiptail --title "Squeezelite Setup | Coenraad Human " --menu "Main Menu:" 20 60 10 \
 "1" "Install Squeezelite" \
-"2" "Update Squeezelite" \
-"3" "Change default audio device" \
-"4" "Change name of player" 3>&1 1>&2 2>&3)
+"2" "Change default audio device" \
+"3" "Change name of player" 3>&1 1>&2 2>&3)
 
 exitstatus=$?
 if [ $exitstatus = 0 ]
@@ -22,13 +21,10 @@ then
 	if [ $menu = 1 ]; then
 		chmod +x /usr/share/squeeze_files/setup/scripts/squeeze_install.sh
 		/usr/share/squeeze_files/setup/scripts/squeeze_install.sh
-	elif [ $menu = 2]; then
-		chmod +x /usr/share/squeeze_files/setup/scripts/squeeze_update.sh
-		/usr/share/squeeze_files/setup/scripts/squeeze_update.sh
-	elif [ $menu = 3 ]; then
+	elif [ $menu = 2 ]; then
 		chmod +x /usr/share/squeeze_files/setup/scripts/squeeze_audio.sh
 		/usr/share/squeeze_files/setup/scripts/squeeze_audio.sh
-	elif [ $menu = 4 ]; then
+	elif [ $menu =3 ]; then
 		chmod +x /usr/share/squeeze_files/scripts/squeeze_name.sh
 		/usr/share/squeeze_files/setup/scripts/squeeze_name.sh
 	fi
