a42ac2f24cfd7a7caeebc220545af9661a8b9fb3
coenraadhuman <coen.human@gmail.com>
2016-10-08 12:37:47 +0200

    Bug fix
    Minor bug fix

--- scripts/boards/squeeze_boards_menu.sh
+++ scripts/boards/squeeze_boards_menu.sh
@@ -6,7 +6,8 @@ title=$(cat /usr/share/squeeze_files/setup/version)
 #------------------------------------
 menu=$(eval `resize` && whiptail --title "$title" --menu "Main Menu:" $LINES $COLUMNS $(( $LINES - 10 )) \
 "1" "Orange Pi" \
-"2" "Raspberry Pi" 3>&1 1>&2 2>&3)
+"2" "Raspberry Pi" \
+"3" "Back" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]
 then
@@ -14,6 +15,8 @@ then
 		/usr/share/squeeze_files/setup/scripts/boards/squeeze_boards_opi.sh
 	elif [ $menu = 2 ]; then
     /usr/share/squeeze_files/setup/scripts/boards/squeeze_boards_rpi.sh
+	elif [ $menu = 3 ]; then
+		squeeze_setup
   fi
 else
 	exit
