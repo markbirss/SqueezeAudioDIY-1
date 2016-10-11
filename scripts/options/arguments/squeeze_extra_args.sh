a4525e430457e3f68315f1408111c840d8b3be50
coenraadhuman <coen.human@gmail.com>
2016-10-11 13:14:06 +0200

    Version 1.3
    Arguments feature working 100%

--- scripts/options/arguments/squeeze_extra_args.sh
+++ scripts/options/arguments/squeeze_extra_args.sh
@@ -10,9 +10,10 @@ view_settings () {
 }
 
 args_changer () {
-	sed -i '19s:.*:SB_EXTRA_ARGS=cha-d all=debug -f /var/log/squeezeaudiodiy/squeezelite.log:' /etc/default/squeezelite
+	sed -i '19s:.*:SB_EXTRA_ARGS=cha'$1' -d all=debug -f /var/log/squeezeaudiodiy/squeezelite.log:' /etc/default/squeezelite
 	sed -i '19s:$:":' /etc/default/squeezelite
 	sed -i '19s:cha:":' /etc/default/squeezelite
+	sed -i '19s:+:\ :g' /etc/default/squeezelite 
 }
 
 argumentsfile=$(cat /usr/share/squeeze_files/setup/files/squeeze_argumentsfile.txt)
