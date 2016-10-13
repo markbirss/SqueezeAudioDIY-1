fb25a1581ac7cf2aa5f7610f3c17ce23052f97c0
coenraadhuman <coen.human@gmail.com>
2016-10-13 16:43:21 +0200

    Orange Pi
    Armbian fix for I2S coded

--- scripts/fixes/armbian/i2s_sbc_opi.sh
+++ scripts/fixes/armbian/i2s_sbc_opi.sh
@@ -2,7 +2,7 @@
 title=$(cat /usr/share/sadiy_files/setup/version)
 
 #------------------------------------
-#REBOOT
+#FUNCTION
 #------------------------------------
 rebootpi () {
   if (whiptail --title "$title" --yes-button "Reboot" --no-button "Exit" --yesno "Reboot is neccessary, would you like to reboot the system?" 10 60) then
@@ -12,6 +12,12 @@ rebootpi () {
     exit
   fi
 }
+
+bin2fex /boot/script.bin /usr/share/sadiy_files/tmp/script.fex
+sed -i 's:.*twi_used.*:twi_used = 0:' /usr/share/sadiy_files/tmp/script.fex
+sed -i 's:.*daudio_used.*:daudio_used = 1:' /usr/share/sadiy_files/tmp/script.fex
+fex2bin /usr/share/sadiy_files/tmp/script.fex /boot/script.bin
+rebootpi
 
 #------------------------------------
 #RESOURCES
