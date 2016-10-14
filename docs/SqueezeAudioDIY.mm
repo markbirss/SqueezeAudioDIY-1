abcd7968a1998a2fa1976e1796bc03d9513d6fb9
coenraadhuman <coen.human@gmail.com>
2016-10-14 12:58:24 +0200

    Mind Map

---
+++ docs/SqueezeAudioDIY.mm
@@ -0,0 +1,192 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="SqueezeAudioDIY" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1476441102544"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="1"/>
+<font ITALIC="true"/>
+<node TEXT="Installers" POSITION="right" ID="ID_810126824" CREATED="1476437507409" MODIFIED="1476440931446" HGAP="70" VSHIFT="-90">
+<edge COLOR="#ff0000"/>
+<node TEXT="SADIY" ID="ID_1239551396" CREATED="1476437914315" MODIFIED="1476442221323" VSHIFT="-10">
+<icon BUILTIN="unchecked"/>
+</node>
+<node TEXT="Sqeezelite" ID="ID_1489995422" CREATED="1476437970913" MODIFIED="1476442209907">
+<icon BUILTIN="checked"/>
+<node TEXT="1.8.5-802" ID="ID_1188144939" CREATED="1476438039900" MODIFIED="1476438390051">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="Latest" ID="ID_1733769675" CREATED="1476438134442" MODIFIED="1476438392787">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node TEXT="Logitech Media Server" ID="ID_924770295" CREATED="1476437985506" MODIFIED="1476442216267">
+<icon BUILTIN="unchecked"/>
+<node TEXT="7.7.5" ID="ID_551134950" CREATED="1476438005882" MODIFIED="1476442227531">
+<icon BUILTIN="unchecked"/>
+</node>
+<node TEXT="7.8.X" ID="ID_1579467407" CREATED="1476438016386" MODIFIED="1476440828517">
+<icon BUILTIN="idea"/>
+</node>
+<node TEXT="7.9.X" ID="ID_1497594438" CREATED="1476438022409" MODIFIED="1476442231363">
+<icon BUILTIN="unchecked"/>
+</node>
+</node>
+</node>
+<node TEXT="Scripts to run after Installers" POSITION="right" ID="ID_1132967119" CREATED="1476441078334" MODIFIED="1476441102544" HGAP="70" VSHIFT="-120">
+<edge COLOR="#ff0000"/>
+<node TEXT="Automated fixes" ID="ID_134141738" CREATED="1476441104942" MODIFIED="1476441325164">
+<icon BUILTIN="idea"/>
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_323877737" STARTINCLINATION="-130;-89;" ENDINCLINATION="-183;-85;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<node TEXT="Determine" ID="ID_1562517007" CREATED="1476441342538" MODIFIED="1476442547971">
+<node TEXT="Distro" ID="ID_450947340" CREATED="1476442551312" MODIFIED="1476442555198"/>
+<node TEXT="Achitecture" ID="ID_1899740379" CREATED="1476442556746" MODIFIED="1476442562058"/>
+</node>
+<node TEXT="Apply" ID="ID_70690302" CREATED="1476442597512" MODIFIED="1476442602062"/>
+</node>
+<node TEXT="Squeezelite options" ID="ID_1171884647" CREATED="1476441224412" MODIFIED="1476441332916">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_448282194" STARTINCLINATION="100;71;" ENDINCLINATION="48;-64;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Removers" POSITION="right" ID="ID_230391171" CREATED="1476437518592" MODIFIED="1476437841603" HGAP="70" VSHIFT="30">
+<edge COLOR="#0000ff"/>
+<node TEXT="SADIY" ID="ID_441191075" CREATED="1476438317652" MODIFIED="1476442245755">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Squeezelite" ID="ID_1403138078" CREATED="1476438334634" MODIFIED="1476442249355">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Logitech Media Server" ID="ID_1064933795" CREATED="1476438342642" MODIFIED="1476442253819">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Complete" ID="ID_187799889" CREATED="1476438353482" MODIFIED="1476439727700">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node TEXT="Fixes" POSITION="left" ID="ID_323877737" CREATED="1476438409410" MODIFIED="1476440806708" HGAP="60" VSHIFT="-150">
+<edge COLOR="#007c7c"/>
+<node TEXT="Master" ID="ID_1576513930" CREATED="1476440647714" MODIFIED="1476440651231">
+<node TEXT="Board will determine limit" ID="ID_1417363370" CREATED="1476440661730" MODIFIED="1476440700645"/>
+</node>
+<node TEXT="Slave" ID="ID_251099291" CREATED="1476440651738" MODIFIED="1476440806708" VSHIFT="20"/>
+</node>
+<node TEXT="Logs" POSITION="left" ID="ID_547623406" CREATED="1476438426581" MODIFIED="1476440799767" HGAP="110" VSHIFT="-20">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Installers" ID="ID_1299258289" CREATED="1476438662611" MODIFIED="1476441460642">
+<node TEXT="Squeezelite" ID="ID_916375202" CREATED="1476440743719" MODIFIED="1476442376226">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Logitech Media Server" ID="ID_59659057" CREATED="1476440753702" MODIFIED="1476442383642">
+<icon BUILTIN="unchecked"/>
+</node>
+<node TEXT="SADIY" ID="ID_1298706881" CREATED="1476440763814" MODIFIED="1476442373290">
+<icon BUILTIN="checked"/>
+</node>
+</node>
+<node TEXT="Playback" ID="ID_1137898877" CREATED="1476438674699" MODIFIED="1476440799766" VSHIFT="30">
+<node TEXT="Squeezelite feature" ID="ID_624904896" CREATED="1476440781304" MODIFIED="1476442369978">
+<icon BUILTIN="checked"/>
+</node>
+</node>
+</node>
+<node TEXT="Squeezelite Options" POSITION="right" ID="ID_805378141" CREATED="1476438449932" MODIFIED="1476439392523" HGAP="70" VSHIFT="90">
+<edge COLOR="#00ff00"/>
+<node TEXT="Original" ID="ID_1265928346" CREATED="1476438702749" MODIFIED="1476440914175">
+<node TEXT="Should be applied on install" ID="ID_448282194" CREATED="1476438843571" MODIFIED="1476438853026">
+<node TEXT="Arguments" ID="ID_476173276" CREATED="1476439608035" MODIFIED="1476442292979">
+<icon BUILTIN="unchecked"/>
+</node>
+<node TEXT="User input" ID="ID_874506549" CREATED="1476439640126" MODIFIED="1476442300075">
+<icon BUILTIN="checked"/>
+<node TEXT="Name" ID="ID_979005006" CREATED="1476439616683" MODIFIED="1476442303995">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Audio" ID="ID_785613350" CREATED="1476439612572" MODIFIED="1476442307139">
+<icon BUILTIN="checked"/>
+</node>
+</node>
+</node>
+<node TEXT="Feature of reverting to orginal state" ID="ID_355265655" CREATED="1476438797659" MODIFIED="1476440914174" VSHIFT="30">
+<node TEXT="Arguments" ID="ID_561678684" CREATED="1476439153011" MODIFIED="1476442319834">
+<icon BUILTIN="checked"/>
+<node TEXT="Arguments will be set to debug and log Squeezelite to file" ID="ID_1407975047" CREATED="1476439512899" MODIFIED="1476439538604"/>
+</node>
+<node TEXT="Name" ID="ID_478658290" CREATED="1476439161667" MODIFIED="1476442339178">
+<icon BUILTIN="unchecked"/>
+<node TEXT="Localhost will be original name" ID="ID_555507009" CREATED="1476439404371" MODIFIED="1476439459044"/>
+</node>
+<node TEXT="Audio" ID="ID_852875807" CREATED="1476439760867" MODIFIED="1476439824388">
+<icon BUILTIN="closed"/>
+</node>
+<node TEXT="Server" ID="ID_468108468" CREATED="1476439164579" MODIFIED="1476442334522">
+<icon BUILTIN="checked"/>
+<node TEXT="Server will be set to be inactive" ID="ID_760782353" CREATED="1476439493947" MODIFIED="1476439508547"/>
+</node>
+</node>
+</node>
+<node TEXT="User input" ID="ID_795592247" CREATED="1476438692171" MODIFIED="1476439352354">
+<node TEXT="Arguments" ID="ID_891681444" CREATED="1476438584690" MODIFIED="1476442359946">
+<icon BUILTIN="unchecked"/>
+</node>
+<node TEXT="Audio" ID="ID_703819020" CREATED="1476438594789" MODIFIED="1476442348818">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Name" ID="ID_124562379" CREATED="1476438603845" MODIFIED="1476442352530">
+<icon BUILTIN="checked"/>
+</node>
+<node TEXT="Server" ID="ID_1149425413" CREATED="1476438599548" MODIFIED="1476442355810">
+<icon BUILTIN="checked"/>
+</node>
+</node>
+</node>
+<node TEXT="Mount" POSITION="left" ID="ID_969281739" CREATED="1476438610443" MODIFIED="1476439907708" HGAP="40" VSHIFT="180">
+<icon BUILTIN="idea"/>
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
