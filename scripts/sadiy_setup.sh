5b2d614d1e5d379ac1f2f22a281b712003066d7d
coenraadhuman <coen.human@gmail.com>
2016-10-22 13:31:07 +0200

    Menu layout
    Old menu removed
    New menu active

--- scripts/sadiy_setup.sh
+++ scripts/sadiy_setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 title=$(cat /usr/share/sadiy_files/setup/version)
 
-#LOG FILE NAME
-logname=$(date +"%Y%m%d.%H%M%S")
-
 #------------------------------------
 #ENSURE RUNNING AS ROOT
 #------------------------------------
@@ -15,36 +12,27 @@ fi
 #MENU
 #------------------------------------
 menu=$(eval `resize` && whiptail --title "$title" --menu "Main Menu:" $LINES $COLUMNS $(( $LINES - 10 )) \
-"1" "Re-install Squeezelite v1.8.5-802" \
-"2" "Install latest Squeezelite available" \
-"3" "Install stable Logitech Media Server 7.7.5" \
-"4" "Install latest nightly Logitech Media Server 7.9.X" \
-"5" "Squeezelite audio device" \
-"6" "Squeezelite options" \
-"7" "View logs" \
-"8" "Fixes for I2S use" \
-"9" "Uninstall software" 3>&1 1>&2 2>&3)
+"1" "Installers" \
+"2" "Squeezelite options" \
+"3" "Logitech Media Server options" \
+"4" "Logs" \
+"5" "Update SqueezeAudioDIY" \
+"6" "Uninstall software" 3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]
 then
 	if [ $menu = 1 ]; then
-		/usr/share/sadiy_files/setup/scripts/install/squeeze/squeeze_reinstall.sh 2>&1 | tee /var/log/squeezeaudiodiy/sque_re-install.log_$logname
+		/usr/share/sadiy_files/setup/scripts/install/install_menu.sh
 	elif [ $menu = 2 ]; then
-		/usr/share/sadiy_files/setup/scripts/install/squeeze/squeeze_install_latest.sh 2>&1 | tee /var/log/squeezeaudiodiy/squeeze_latest_.log_$logname
-  elif [ $menu = 3 ]; then
-  	/usr/share/sadiy_files/setup/scripts/install/lms/lms_install.sh 2>&1 | tee /var/log/squeezeaudiodiy/logicms_install.log_$logname
-  elif [ $menu = 4 ]; then
-  	/usr/share/sadiy_files/setup/scripts/install/lms/lms_install_latest.sh 2>&1 | tee /var/log/squeezeaudiodiy/logicms_latest_.log_$logname
-  elif [ $menu = 5 ]; then
-    /usr/share/sadiy_files/setup/scripts/options/audio/squeeze_audio.sh
-  elif [ $menu = 6 ]; then
 		/usr/share/sadiy_files/setup/scripts/options/squeeze_options_menu.sh
-  elif [ $menu = 7 ]; then
-    /usr/share/sadiy_files/setup/scripts/logs/logs_menu.sh
-  elif [ $menu = 8 ];then
-    /usr/share/sadiy_files/setup/scripts/fixes/i2s_fixes_menu.sh
-  elif [ $menu = 9 ];then
-    /usr/share/sadiy_files/setup/scripts/remove/sadiy_remove_menu.sh
+  elif [ $menu = 3 ]; then
+  	/usr/share/sadiy_files/setup/scripts/options2/lms_options_menu.sh
+  elif [ $menu = 4 ]; then
+  	/usr/share/sadiy_files/setup/scripts/logs/logs_menu.sh
+  elif [ $menu = 5 ]; then
+    /usr/share/sadiy_files/setup/scripts/sadiy_update.sh
+  elif [ $menu = 6 ]; then
+		/usr/share/sadiy_files/setup/scripts/remove/sadiy_remove_menu.sh
   fi
 else
 	exit
