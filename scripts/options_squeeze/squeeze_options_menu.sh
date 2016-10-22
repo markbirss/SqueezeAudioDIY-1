6c1f40d4d91e69191f3f37679a9b4bc3c8242a48
coenraadhuman <coen.human@gmail.com>
2016-10-23 00:05:23 +0200

    Bug fix

--- scripts/options_squeeze/squeeze_options_menu.sh
+++ scripts/options_squeeze/squeeze_options_menu.sh
@@ -14,12 +14,14 @@ view_settings () {
 #------------------------------------
 menu=$(eval `resize` && whiptail --title "$title" --menu "Menu:" $LINES $COLUMNS $(( $LINES - 10 )) \
 "1" "View current settings" \
-"2" "Set Squeezelite player name" \
-"3" "Set extra arguments to Squeezelite" \
-"4" "Deactivate extra arguments" \
-"5" "Point Squeezelite to your Server via IPv4" \
-"6" "Deactivate custom server IPv4 address" \
-"7" "Back" 3>&1 1>&2 2>&3)
+"2" "View the audio devices in detail" \
+"3" "Change the default audio device" \
+"4" "Set Squeezelite player name" \
+"5" "Set extra arguments to Squeezelite" \
+"6" "Deactivate extra arguments" \
+"7" "Point Squeezelite to your Server via IPv4" \
+"8" "Deactivate custom server IPv4 address" \
+"9" "Back" 3>&1 1>&2 2>&3)
 
 exitstatus=$?
 if [ $exitstatus = 0 ]
@@ -28,16 +30,20 @@ then
 		view_settings
 		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_options_menu.sh
 	elif [ $menu = 2 ]; then
-		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_name.sh
+		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_audio.sh
 	elif [ $menu = 3 ]; then
-		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_extra_args.sh
+		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_audio.sh
 	elif [ $menu = 4 ]; then
-		/usr/share/sadiy_files/setup/scripts/options_squeeze/queeze_args_de.sh
+		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_name.sh
 	elif [ $menu = 5 ]; then
-		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_server.sh
+		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_extra_args.sh
 	elif [ $menu = 6 ]; then
-		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_server_de.sh
+		/usr/share/sadiy_files/setup/scripts/options_squeeze/queeze_args_de.sh
 	elif [ $menu = 7 ]; then
+		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_server.sh
+	elif [ $menu = 8 ]; then
+		/usr/share/sadiy_files/setup/scripts/options_squeeze/squeeze_server_de.sh
+	elif [ $menu = 9 ]; then
 		sadiy_setup
   fi
 else
