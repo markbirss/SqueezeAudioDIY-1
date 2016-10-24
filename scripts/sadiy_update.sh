25974419d45aacff9eba1553016af686aa5c7306
coenraadhuman <coen.human@gmail.com>
2016-10-24 15:27:37 +0200

    Menu
    Update to update feature

--- scripts/sadiy_update.sh
+++ scripts/sadiy_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 title=$(cat /usr/share/sadiy_files/setup/version)
 
-if (whiptail --title "$title" --yes-button "Update" --no-button "Back" --yesno "Are you sure you want to update SqueezeAudioDIY?" 18 60) then
+if (whiptail --title "$title" --yes-button "Update" --no-button "Back" --yesno "Are you sure you want to update latest SqueezeAudioDIY? (Warning! This update may contain unstable release)" 18 60) then
   cd /tmp
   rm -R /tmp/SqueezeAudioDIY > /dev/null 2>&1
   git clone https://github.com/coenraadhuman/SqueezeAudioDIY.git
