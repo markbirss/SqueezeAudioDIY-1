111e790b474c4c4e94149ad19031683e12f4aacb
coenraadhuman <coen.human@gmail.com>
2016-10-24 11:00:55 +0200

    Update
    New method of getting LMS

--- scripts/install/lms_install_7.7.5.sh
+++ scripts/install/lms_install_7.7.5.sh
@@ -2,29 +2,46 @@
 title=$(cat /usr/share/sadiy_files/setup/version)
 
 #------------------------------------
+#FUNCTIONS
+#------------------------------------
+install_lms () {
+  service logitechmediaserver stop
+  install=$(ls | grep logitechmediaserver_7.7.5_all.deb)
+  dpkg -i /usr/share/sadiy_files/installers/lms_stable/$install
+  rm /usr/share/sadiy_files/installers/lms_stable/index*
+  sadiy_setup
+}
+
+download_lms () {
+  wget http://downloads.slimdevices.com/LogitechMediaServer_v7.7.5/ > /dev/null 2>&1
+  exitstatus=$?
+  if [ $exitstatus = 0 ]
+    then
+      echo "[ OK ] INDEX RECEIVED"
+    else
+      echo "[ ERROR ] INDEX DOWNLOAD FAILED"
+  fi
+  download=$(grep logitechmediaserver_....._all.deb ./index.html | cut -c82-114)
+  echo "DOWNLOADING LMS 7.7.5"
+  wget http://downloads.slimdevices.com/LogitechMediaServer_v7.7.5/$download
+  exitstatus=$?
+  if [ $exitstatus = 0 ]
+    then
+      echo "[ OK ] LMS 7.7.5 DOWNLOADED"
+    else
+      echo "[ ERROR ] LMS 7.7.5 DOWNLOAD FAILED"
+  fi
+}
+
+#------------------------------------
 #INSTALL
 #------------------------------------
 cd /usr/share/sadiy_files/installers/lms_stable/
-wget http://downloads.slimdevices.com/LogitechMediaServer_v7.7.5/ > /dev/null 2>&1
-exitstatus=$?
-if [ $exitstatus = 0 ]
+check=$(ls | grep logitechmediaserver_7.7.*_all.deb)
+if [ check = logitechmediaserver_7.7.5_all.deb ]
   then
-    echo "[ OK ] INDEX RECEIVED"
+    install_lms
   else
-    echo "[ ERROR ] INDEX DOWNLOAD FAILED"
+    download_lms
+    install_lms
 fi
-download=$(grep logitechmediaserver_....._all.deb ./index.html | cut -c82-114)
-echo "DOWNLOADING LMS 7.7.5"
-wget http://downloads.slimdevices.com/LogitechMediaServer_v7.7.5/$download
-exitstatus=$?
-if [ $exitstatus = 0 ]
-  then
-    echo "[ OK ] LMS 7.7.5 DOWNLOADED"
-  else
-    echo "[ ERROR ] LMS 7.7.5 DOWNLOAD FAILED"
-fi
-service logitechmediaserver stop
-install=$(ls | grep logitechmediaserver_....._all.deb)
-dpkg -i /usr/share/sadiy_files/installers/lms_stable/$install
-rm /usr/share/sadiy_files/installers/lms_stable/index*
-sadiy_setup
